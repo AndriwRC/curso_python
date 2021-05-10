1453943724c6cbee02dbe362b73922d1d1e7d017
AndriwRC <randriw4@gmail.com>
2021-05-10 17:33:22 -0500

    Prueba de primalidad bajo el método encontrado en internet

--- prueba_primalidad.py
+++ prueba_primalidad.py
@@ -1,14 +1,37 @@
-def es_primo(numero):
+def es_factor_primo(factor_primo):
     contador = 0
 
-    for i in range(1, numero + 1):
-        if numero % i == 0:
+    for j in range(1, factor_primo + 1):
+        if factor_primo % j == 0:
             contador += 1
 
     if contador == 2:
         return True
     else:
         return False
+
+def es_primo(numero):
+    for factor_primo in range(1, numero + 1):
+        if numero == 2:
+            return True
+            break
+            # Sabemos que 2 es primo, pero para comprobralo con rigurosidad se realiza lo siguiente:
+            # if es_factor_primo(numero):
+            #     return True
+            #     break
+            # else:
+            #     return False
+            #     break
+        elif es_factor_primo(factor_primo):
+            cociente = numero // factor_primo
+            residuo = numero % factor_primo
+
+            if cociente <= factor_primo and residuo != 0:
+                return True
+                break
+            elif residuo == 0:
+                return False
+                break
 
 
 def run():
